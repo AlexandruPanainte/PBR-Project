521b32b329a30a2979b4f002c51516392e6c9adc
AdelinaAlicia <62259221+AdelinaAlicia@users.noreply.github.com>
2021-05-13 16:10:34 +0300

    Add files via upload

--- PBRProj/main.py
+++ PBRProj/main.py
@@ -6,12 +6,11 @@ def ProvideText(text,TextInput):
     with open(TextInput,"w")as textInput:
         textInput.write(text)
 
-def UpdateGrammar(text,limbaj,limbajTokenizat,Facts):
+def UpdateGrammar(text,limbaj,limbajTokenizat,FactsUpdate):
     #Punem textul din fisierul limbaj intr-un string
     with open(limbaj,"r") as string:
         data = string.read().replace("\n"," ")
     #Transformam string-ul intr-un dictionar
-    #dataDictionary = eval(str(data))
     # Transformam textul intr-o lista de cuvinte
     cuvinteDinText = text.split()
     # Verificam pentru fiecare cuvant din textul dat ca input
@@ -34,14 +33,42 @@ def UpdateGrammar(text,limbaj,limbajTokenizat,Facts):
     with open(limbajTokenizat, "w") as limbaj:
         limbaj.write(str(tagged))
     #Transformam limbajul tokenizat in fact-uri clips si le adaugam intr-un fisier "Facts"
-    with open(Facts,"w") as string:
+    with open(FactsUpdate,"w") as string:
         for element in tagged:
             text = "(assert (" + element[0] + " " + element[1]+"))"
             string.write(text + "\n")
+
+
+def secondFuncitonality(text,limbaj,limbajTokenizat, FactsVerify):
+    # Punem textul din fisierul limbaj intr-un string
+    with open(limbaj,"r") as string:
+        limbajText = string.read().replace("\n"," ")
+    # Transformam textul intr-o lista de cuvinte
+    verifytext = text.split()
+
+    # Transformam datele din fisierul limbajului intr-o lista de cuvinte
+    cuvinte = limbajText.split()
+    # Verificam pentru fiecare cuvant din textul dat ca input
+    # daca este in lista noastra de cuvinte
+    for word in verifytext:
+        if word not in cuvinte:
+            print("Propozitia data ca input nu poate fi parsata cu limbajul actual.")
+            return
+    print("Propozitia data ca input poate fi parsata cu limbajul actual si va fi\n"
+          "transformata in fact-uri CLIPS")
+
+    tokens = nltk.word_tokenize(text)
+    tagged = nltk.pos_tag(tokens)
+
+    with open(FactsVerify,"w") as string:
+        for element in tagged:
+            text = "(assert (" + element[0] + " " + element[1]+"))"
+            string.write(text + "\n")
+
 
 # Pentru verificarea functionalitatii recomand sa se stearga toate datele din
 # limbaj si limbajTokenizat iar apoi sa se apeleze de mai multe ori functia
 
 
-UpdateGrammar("Alt input test","Limbaj.txt","LimbajTokenizat.txt","Facts.txt")
-ProvideText("Alt input test","TextInput.txt")
+UpdateGrammar("Alt input , test","Limbaj.txt","LimbajTokenizat.txt","FactsUpdate.txt")
+secondFuncitonality("Alt input","Limbaj.txt","LimbajTokenizat.txt","FactsVerify.txt")
